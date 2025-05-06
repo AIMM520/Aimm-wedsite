24fb97b32137904599d3e3a12518438c04d0c9d4
AIMM520 <leexun1072002@gmail.com>
2025-05-06 08:09:54 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 export default function App() {
   return (
-    <div>
-      <h1>欢迎访问 AIMM 官网</h1>
-      <p>我们致力于成为中马跨境的战略桥梁。</p>
+    <div style={{ padding: "2rem", fontSize: "1.5rem" }}>
+      欢迎访问 AIMM 官网
     </div>
   );
 }
